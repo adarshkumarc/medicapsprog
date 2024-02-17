1c913da13dee434f696c81a757f7752123398a60
adarshgurjar744@gmail.com <adarshgurjar744@gmail.com>
2024-02-17 14:12:20 +0530

    basic programs

---
+++ pythonlab/atmcurrency.py
@@ -0,0 +1,7 @@
+
+name=input("enter the name :")
+address=input("enter the address : ")
+mobilenumber=input("enter the mobile number :")
+print("Name-",name)
+print("address-",address)
+print("mobilenumber-",mobilenumber)
