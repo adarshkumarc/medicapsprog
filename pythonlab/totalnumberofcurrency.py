94465a664e7e79ed5944371ab62e1a6c90f1c187
adarshgurjar744@gmail.com <adarshgurjar744@gmail.com>
2024-02-17 15:16:12 +0530

    atm ]praogrma

---
+++ pythonlab/totalnumberofcurrency.py
@@ -0,0 +1,4 @@
+note1=int(input("entr the 100 notes"))
+note2=int(input("enter the 200 note"))
+note3=int(input("enter the 500 notes"))
+print( "total aamount in account",note1*100+note2*200+note3*500)
